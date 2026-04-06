b12c59d711585e8458246dee7f89b1a8bbfe9890
jack303 <jackov3000gmail.com>
2026-04-06 10:21:21 +0600

    Homework 2 complete

---
+++ apps/nurai/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'city', 'email', 'created_at')
+
+# Register your models here.
