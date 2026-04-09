1b38f9bf2c23de8889471eb2967368323d48f7d4
jack303 <jackov3000gmail.com>
2026-04-09 15:35:04 +0600

    Добавлен шаблон викторины

--- Love/urls.py
+++ Love/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from apps.nurai.views import home_page
+from apps.nurai.views import home_page, quiz_page, about_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +27,12 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+ # Импортируй новые функции
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home_page, name='home'),
+    path('quiz/', quiz_page, name='quiz'),
+    path('about/', about_page, name='about'),
+]
--- apps/nurai/views.py
+++ apps/nurai/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    html_content = """
-    <h1>Моя страница</h1>
-    <p>Привет, это моё фото:</p>
-    <img src="/media/photos/me.jpg" alt="Моё фото" width="400">
-    """
-    return HttpResponse(html_content)
+    return render(request, 'index.html')
+
+def quiz_page(request):
+    return render(request, 'quiz.html')
+
+def about_page(request):
+    return render(request, 'about.html')
 
 # Create your views here.
