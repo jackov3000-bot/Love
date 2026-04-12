2dc47d640d55d283674a4835ef2936d3143bb6e0
jack303 <jackov3000gmail.com>
2026-04-12 13:41:43 +0600

    Сделал сайт для фотографий

--- apps/nurai/views.py
+++ apps/nurai/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return render(request, 'index.html')
+    return render(request, 'index1.html')
 
 def quiz_page(request):
     return render(request, 'quiz.html')
